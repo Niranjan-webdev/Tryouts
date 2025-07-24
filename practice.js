b6a5b309d9debd35caa47e713afe9edc6b0045c5
Niranjan <63530958+Niranjan-webdev@users.noreply.github.com>
2025-07-24 12:28:06 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,60 @@
+// let value = "aabbcdeff"
+// let nonRepeatingChar = (str) => {
+//   for (let i = 0; i < str.length; i++) {
+//     let char = str[i];
+//     console.log('-->',char, str.indexOf(char), str.lastIndexOf(char) )
+//     if (str.indexOf(char) === str.lastIndexOf(char)) {
+//       console.log("First non-repeating character:", char);
+//       return;
+//     }
+//   }
+//   console.log("No non-repeating character found");
+// };
+// nonRepeatingChar(value)
+
+// let arr = [1, 2, 2, 3, 4, 4, 5];
+
+// arr.filter(v => v == 5)
+
+// let arr1 = [4,13, 34, 8, 1]
+// let arr2 = [3, 5, 7, 9, 6]
+
+// let findMedian = (num1, num2) => {
+//   let array = [...arr1, ...arr2]
+//   array = array.sort((a, b) => a - b);
+//   console.log('array-->', array)
+//   if (array.length % 2 == 0) {
+//     let value = array.length / 2
+//     let median = (array[value] + array[value-1])/2;
+//     console.log('Median value',median)
+//   } else {
+//     let value = array.length / 2
+//     console.log('Median value of array', Math.ceil(value), array[Math.ceil(value)])
+//   }
+
+// }
+// findMedian(arr1, arr2)
+
+// let nums = [0,4,3,0], target = 0;
+
+// var twoSum = function (nums, target) {
+//   const hash = {};
+//   let n = nums.length;
+//   for (let i = 0; i < n; i++) {
+//       const complement = target - nums[i];
+//       console.log('complement', complement, hash)
+//       if (complement in hash) {
+//         console.log('return', hash, [hash[complement], i])
+//           return [hash[complement], i];
+//       }hash[nums[i]] = i;
+//       console.log('hash', hash)
+//   }return [];
+// };
+// twoSum(nums,target)
+
+// remove duplicates from an array
+
+let arr = [1, 2, 2, 3, 4, 4, 5, 1, 5, 4];
+let array= arr.filter((v,i) => {
+console.log(arr.indexOf(v),i)
+})
