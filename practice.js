03b9f9198fe820e6873a7b664e10d52a7f48c47e
Niranjan <63530958+Niranjan-webdev@users.noreply.github.com>
2025-07-25 13:21:04 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -55,6 +55,4 @@
 // remove duplicates from an array
 
 let arr = [1, 2, 2, 3, 4, 4, 5, 1, 5, 4];
-let array= ar.foreac((v,i) => {
-return [i].i let name= 'niranjan'
-})
+let array= arr.filter((v => v==5})
